247b25d4070a0df1d976654911fa000a62cd2162
Djhoud <luizoxvaldo@gmail.com>
2024-10-30 01:29:55 -0300

    0.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Header from "./components/Header";
+
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 
@@ -10,7 +10,7 @@ import "./styles/index.css";
 function App() {
   return (
     <>
-      <Header />
+      
       <Hero />
       <About />
       <Projects />
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,14 +4,15 @@ const Hero = () => {
   return (
     <section className="hero">
       <div className="hero-content">
-        <h1 className="hero-title">
-          <span className="react">REACT</span> 
-          <span className="developer">DEVELOPER</span>
-        </h1>
+        <div className="vignette">
+          <h1 className="hero-title">
+            <span className="react">REACT</span> 
+            <span className="developer">DEVELOPER</span>
+          </h1>
+        </div>
         <p className="hero-subtitle">
           Tenho 20 anos e sou desenvolvedor frontend. Gosto de resolver problemas usando design e tecnologia.
         </p>
-    
       </div>
     </section>
   );
